fdcdfff892799ca268de9124f91479b6f928c707
Deepanshu30verma <deepanshuverma@rightpoint.com>
2023-08-14 11:10:27 +0530

    React-dependency

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p>Hey How are you we are in the react dom</p>
+    </>
+  );
+}
+
+export default App;
